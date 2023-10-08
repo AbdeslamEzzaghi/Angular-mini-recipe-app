4b2baa3ec2edb18ed5b6cf2c9e5a745cb0ba6937
Abdeslam Ezzaghi <60900845+AbdeslamEzzaghi@users.noreply.github.com>
2023-10-08 14:23:47 +0100

    modified the dropdown directive to open when clicking on the host element and closes when clicked anywhere else

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
@@ -7,9 +7,19 @@ export class DropdownDirective {
 
   @HostBinding('class.open') isOpen : boolean = false;
 
+  @HostListener('document:click',['$event']) onToggleDropdown(){
+    this.isOpen = (this.elRef.nativeElement.contains(event.target))?!this.isOpen:false;
+  }
+  constructor(private elRef : ElementRef) { }
+
+}
+
+/*
+
+  @HostBinding('class.open') isOpen : boolean = false;
+
   @HostListener('click') onToggleDropdown(){
     this.isOpen = !this.isOpen;
   }
   constructor() { }
-
-}
+*/
